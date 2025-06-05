30e48eed018fb153899f8bb2628b9bb6c9fdc676
Klebson <klebson.stayner@gmail.com>
2025-06-05 10:37:46 +0100

    first commit

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require('../controllers/bookController');
+
+router.post('/', bookController.createBook);
+router.get('/', bookController.getAllBooks);
+router.get('/:id', bookController.getBookById);
+router.get('/author/:author_id', bookController.getBooksByAuthor);
+router.put('/:id', bookController.updateBook);
+router.patch('/:id', bookController.patchBook);
+router.delete('/:id', bookController.deleteBook);
+
+module.exports = router;
